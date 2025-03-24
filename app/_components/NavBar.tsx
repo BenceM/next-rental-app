8e51001d613ab83a9a9711a0b390f7e0d233fdb1
Bence <m.bence211@gmail.com>
2025-03-24 17:25:56 +0100

    folder restructuring

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { NAVBAR_HEIGHT } from "../_lib/constants";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/shadcn/components/ui/button";
 
 export default function NavBar() {
 	return (
